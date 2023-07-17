5ed188001c7838759d024d2836345ad9900a1927
Ricardo Delgado <rickydel19@gmail.com>
2023-07-17 12:33:31 -0500

    Added nutritional summary method to meals model

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -6,4 +6,20 @@ class Meal < ApplicationRecord
   def get_meal_ingredients
     @meal_ingredients = MealIngredient.where(meal_id: id)
   end
+
+  def nutritional_summary
+    output_hash = { calories: 0, fat: 0, sodium: 0, carbs: 0, protein: 0, sugar: 0, cholesterol: 0 }
+    MealIngredient.where(meal_id: id).each do |meal_ingredient|
+      ingredient = Ingredient.find_by(id: meal_ingredient.ingredient_id)
+      ingredient_quantity = meal_ingredient.ingredient_quantity
+      output_hash[:calories] += ingredient.calories * ingredient_quantity
+      output_hash[:fat] += ingredient.fat * ingredient_quantity
+      output_hash[:sodium] += ingredient.sodium * ingredient_quantity
+      output_hash[:carbs] += ingredient.carbs * ingredient_quantity
+      output_hash[:protein] += ingredient.protein * ingredient_quantity
+      output_hash[:sugar] += ingredient.sugar * ingredient_quantity
+      output_hash[:cholesterol] += ingredient.cholesterol * ingredient_quantity
+    end
+    return output_hash
+  end
 end
--- app/views/meals/_meal.json.jbuilder
+++ app/views/meals/_meal.json.jbuilder
@@ -9,3 +9,4 @@ json.meal_ingredients meal.meal_ingredients do |meal_ingredient|
   json.ingredient_quantity meal_ingredient.ingredient_quantity
   json.ingredient meal_ingredient.ingredient
 end
+json.nutritional_summary meal.nutritional_summary
