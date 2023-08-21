b53df509d12223a9fb86f389a5c89d963937da81
Ricardo <rickydel19@gmail.com>
2023-08-20 23:47:08 -0500

    Added authentication for user to see ingredients and meals

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -1,5 +1,5 @@
 class IngredientsController < ApplicationController
-  before_action :authenticate_user, except: [:index, :show]
+  before_action :authenticate_user
 
   def index
     @ingredients = Ingredient.all
--- app/controllers/meals_controller.rb
+++ app/controllers/meals_controller.rb
@@ -1,5 +1,5 @@
 class MealsController < ApplicationController
-  before_action :authenticate_user, except: [:index, :show]
+  before_action :authenticate_user
 
   def index
     @meals = Meal.all
