9b058c712c46e632988c8e80a19a66fd49ad7915
Ricardo <rickydel19@gmail.com>
2023-08-20 23:45:31 -0500

    Removed validations for ingredient names

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,6 +1,5 @@
 class Ingredient < ApplicationRecord
   belongs_to :user
-  validates :name, uniqueness: true, presence: true
   has_many :meal_ingredients
   has_many :meals, through: :meal_ingredients
 
